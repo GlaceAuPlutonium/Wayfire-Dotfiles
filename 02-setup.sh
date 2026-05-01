0a2075de385b6b69cb8e8ccec8f70b16642888e9
Gap <139619750+GlaceAuPlutonium@users.noreply.github.com>
2026-05-01 20:09:19 +0200

    Create 02-setup.sh

---
+++ 02-setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Setting up annoying system related things..."
+
+# Noctalia
+read -p "Create Noctalia Shell repository ? [y/n] : " choice
+if [[ "$choice" == "y" ]]; then
+  echo "Creating Noctalia Shell repository..."
+  echo "repository=https://universalrepo.r1xelelo.workers.dev/void" | sudo tee /etc/xbps.d/10-noctalia.conf
+else
+  echo "Skipped Noctalia Shell repository creation."
+fi
+read -p "Install Noctalia Shell ? [y/n] : " choice
+if [[ "$choice" == "y" ]]; then
+  echo "Installing Noctalia Shell..."
+  sudo xbps-install -S
+  sudo xbps-install noctalia-shell
+else
+  echo "Skipped Noctalia Shell installation."
+fi
+
+# Pipewire
+read -p "Set up Pipewire ? [y/n] : " choice
+if [[ "$choice" == "y" ]]; then
+  echo "Setting up Pipewire..."
+  sudo usermod -aG audio,video gap
+  sudo mkdir -p /etc/pipewire/pipewire.conf.d
+  sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+  sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+else
+  echo "Skipped Pipewire setup."
+fi
+
+echo "Done setting up annoying little things."
