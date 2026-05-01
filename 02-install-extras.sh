93476d881a54bc211698a337769059f5bd79fd73
Gap <139619750+GlaceAuPlutonium@users.noreply.github.com>
2026-05-01 19:18:12 +0200

    Update 02-install-extras.sh

--- 02-install-extras.sh
+++ 02-install-extras.sh
@@ -4,51 +4,47 @@ set -e
 
 echo "Installing themes and extras..."
 
-WORKDIR=$(mktemp -d)
-cd "$WORKDIR"
-
 # TokyoNight GTK Theme
-git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme
-cd Tokyonight-GTK-Theme/themes
-sudo ./install.sh -n TokyoNight -c dark -l --tweaks black
-sudo ./install.sh -n TokyoNight -c dark -s compact -l --tweaks black
-cd "$WORKDIR"
+read -p "Install Tokyonight GTK Theme ? [y/n] : " choice
+if [[ "$choice" == "y" ]]; then
+  echo "Installing GTK theme..."
+  git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme
+  (cd Tokyonight-GTK-Theme/themes && sudo ./install.sh -n TokyoNight -c dark -l --tweaks black && sudo ./install.sh -n TokyoNight -c dark -s compact -l --tweaks black)
+else
+  echo "Skipped GTK theme installation."
+fi
 
 # MoreWaita Icon Theme
-git clone https://github.com/somepaulo/MoreWaita.git
-cd MoreWaita
-sudo ./install.sh
-cd "$WORKDIR"
+read -p "Install MoreWaita icon theme ? [y/n] : " choice
+if [[ "$choice" == "y" ]]; then
+  echo "Installing icon theme..."
+  git clone https://github.com/somepaulo/MoreWaita.git
+  (cd MoreWaita && sudo ./install.sh)
+else
+  echo "Skipped icon theme installation."
+fi
+
 
 # Phinger Cursor Theme - Commented for now because last time I tried I didn't work so I gotta check what's wrong
 # wget -cO- https://github.com/phish/phinger-cursors/releases/latest/download/phinger-cursors-variants.tar.bz2 | sudo tar xfj - -C /usr/share/icons
 
-# SDDM Astronaut Theme
-sudo git clone -b master --depth 1 https://github.com/keyitdev/sddm-astronaut-theme.git \
-/usr/share/sddm/themes/sddm-astronaut-theme
-
-sudo cp -r /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts
-
-sudo cp extras/sddm.conf /etc/sddm.conf
-sudo cp extras/env.conf /etc/sddm.conf.d/env.conf
-
-echo -e "[General]\nInputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
-
-sudo fc-cache -fv
-
-sudo cp extras/hyprland_kath.conf \
-/usr/share/sddm/themes/sddm-astronaut-theme/Themes/
-
-sudo sed -i 's|^ConfigFile=.*|ConfigFile=Themes/hyprland_kath.conf|' \
-/usr/share/sddm/themes/sddm-astronaut-theme/metadata.desktop
-
 # Antidote
-git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
+read -p "Install Antidote ? [y/n] : " choice
+if [[ "$choice" == "y" ]]; then
+  echo "Installing Antidote..."
+  git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
+else
+  echo "Skipped Antidote installation."
+fi
 
 # CyberGRUB
-git clone https://github.com/adnksharp/CyberGRUB-2077
-cd CyberGRUB-2077
-sudo $SHELL ./install.sh -L samurai
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+read -p "Install CyberGRUB Theme ? [y/n] : " choice
+if [[ "$choice" == "y" ]]; then
+  echo "Installing CyberGRUB-2077 GRUB theme..."
+  git clone https://github.com/adnksharp/CyberGRUB-2077
+  (cd CyberGRUB-2077 && sudo $SHELL ./install.sh -L samurai && sudo grub-mkconfig -o /boot/grub/grub.cfg)
+else
+  echo "Skipped GRUB theme installation."
+fi
 
 echo "Extras installed."
